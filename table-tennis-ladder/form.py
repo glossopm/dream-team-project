c8d479093fd24725b2fbc96a3d1af1268b477a05
Malik Glossop <malik.glossop@infinityworks.com>
2018-09-26 15:00:30 +0100

    Changed from 20 char to 14 char, added image, removed some entries from database

--- table-tennis-ladder/form.py
+++ table-tennis-ladder/form.py
@@ -5,16 +5,16 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class AddLeaderboardForm(FlaskForm):
     leaderboard = StringField('Leaderboard',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=14)])
     submit1 = SubmitField('Add Leaderboard')
 
 class RecordMatchForm(FlaskForm):
-    winner = StringField('Winner', validators=[DataRequired(), Length(min=2, max=20)])
-    loser = StringField('Loser', validators=[DataRequired(), Length(min=2, max=20)])
+    winner = StringField('Winner', validators=[DataRequired(), Length(min=2, max=14)])
+    loser = StringField('Loser', validators=[DataRequired(), Length(min=2, max=14)])
     submit2 = SubmitField('Record Match')
     
 class AddPlayerForm(FlaskForm):
     player = StringField('Player',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=14)])
     submit3 = SubmitField('Add Player')
 
